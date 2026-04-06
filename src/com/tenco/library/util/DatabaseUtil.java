b1790177942af64b76e5925dada972466d0d1d08
mskimdev <aksen1754@naver.com>
2026-04-06 14:33:17 +0900

    [feat] DatabaseUtil 생성

--- src/com/tenco/library/util/DatabaseUtil.java
+++ src/com/tenco/library/util/DatabaseUtil.java
@@ -1,4 +1,54 @@
 package com.tenco.library.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseUtil {
+
+
+    private static final String URL = "jdbc:mysql://localhost:3306/library";
+
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PWD = System.getenv("DB_PASSWORD");
+
+
+    // 새로운 DB 연결 객체를 반환
+    public static Connection getConnection() throws SQLException {
+
+        // 재미삼아 효과 만들어 보기
+        Thread thread = new Thread(() -> {
+            System.out.print("Connecting to database");
+            for (int i = 0; i < 5; i++) {
+                System.out.print(".");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println();
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection conn = DriverManager.getConnection(URL, DB_USER, DB_PWD);
+
+        System.out.println(conn.getMetaData().getDatabaseProductName());
+        System.out.println(conn.getMetaData().getDatabaseProductVersion());
+
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            DatabaseUtil.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/com/tenco/library/view/LibraryView.java
+++ src/com/tenco/library/view/LibraryView.java
@@ -1,4 +1,25 @@
 package com.tenco.library.view;
 
-public class LibraryView {
+import javax.swing.*;
+
+public class LibraryView extends JFrame {
+    private JPanel mainPanel;
+
+
+    // 실행 순서 정의
+    public void run() {
+        setInitData();
+        setInitLayout();
+    }
+
+    // 데이터 초기화
+    public void setInitData(){
+
+    }
+
+    // 레이아웃 설정
+    public void setInitLayout(){}
+
+
+
 }
